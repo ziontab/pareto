523c6ac3db185bb77482a4e24c9a437069b6bb14
Dinar Sabitov <lolall@yandex.ru>
2013-04-17 11:39:57 +0400

    new urls scheme, .pyc files ignore

--- pareto/urls.py
+++ pareto/urls.py
@@ -4,11 +4,25 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # User accounts
     url(r'^login/$',    'parapp.views.login'),
     url(r'^register/$', 'parapp.views.register'),
-    url(r'^admin/', include(admin.site.urls)),
+    
+    # Projects
+    url(r'^projects/$', 'parapp.views.list_project'),
+    url(r'^project/(?P<project_id>\d+)/$', 'parapp.views.get_project'),
+    url(r'^ajax/project/create/$', 'parapp.views.create_projects'),
 
-    # url(r'^$', 'pareto.views.home', name='home'),
-    # url(r'^pareto/', include('pareto.foo.urls')),
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Calculations
+    url(r'^calculations/(?P<project_id>\d+)/$', 'parapp.views.list_calculation'),
+    url(r'^calculation/(?P<calculation_id>\d+)/$', 'parapp.views.get_calculation'),
+    url(r'^calculation/create/$', 'parapp.views.create_calculation'),
+
+    # Estimations
+    url(r'^estimations/(?P<project_id>\d+)/$', 'parapp.views.list_estimation'),
+    url(r'^estimation/(?P<estimation_id>\d+)/$', 'parapp.views.get_estimation'),
+    url(r'^estimation/create/$', 'parapp.views.create_estimation'),
+
+    # Admin
+    url(r'^admin/',     include(admin.site.urls)),
 )
