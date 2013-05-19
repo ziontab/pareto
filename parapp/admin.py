22a6a9d1283fdeb18c49d4eae164605173fb1f84
Dinar Sabitov <lolall@yandex.ru>
2013-05-19 17:46:32 +0400

    editing tables with admin

---
+++ parapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from parapp.models import Project, Calculation, Estimation, Problem, Algorithm
+
+admin.site.register(Project)
+admin.site.register(Calculation)
+admin.site.register(Estimation)
+admin.site.register(Problem)
+admin.site.register(Algorithm)
