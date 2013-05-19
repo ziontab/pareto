f770daf60ab892304e1fc1aa5bbe5e7f9abf185f
Dinar Sabitov <lolall@yandex.ru>
2013-05-19 17:43:01 +0400

    model date auto_add, __unucode__ name

--- parapp/models.py
+++ parapp/models.py
@@ -5,26 +5,34 @@ class Project(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=255)
     date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Problem(models.Model):
     name = models.CharField(max_length=255)
-    path = models.FilePathField(path=None, max_length=255)
-    date = models.DateTimeField('date published')
+    path = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
     is_binary = models.BooleanField()
+    def __unicode__(self):
+        return self.name
 
 class Algorithm(models.Model):
     name = models.CharField(max_length=255)
     path = models.FilePathField(path=None, max_length=255)
-    date = models.DateTimeField('date published')
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Calculation(models.Model):
     project     = models.ForeignKey(Project)
     algorithm   = models.ForeignKey(Algorithm)
     name        = models.CharField(max_length=255)
     time        = models.IntegerField(default=0)
-    date        = models.DateTimeField('date executed')
+    date        = models.DateTimeField(auto_now_add=True, blank=True)
     input_data  = models.TextField()
     output_data = models.TextField()
+    def __unicode__(self):
+        return self.name
 
 class Estimation(models.Model):
     project     = models.ForeignKey(Project)
@@ -33,6 +41,8 @@ class Estimation(models.Model):
     calc_2      = models.ForeignKey(Calculation, null=True, related_name='calc_2')
     name        = models.CharField(max_length=255)
     time        = models.IntegerField(default=0)
-    date        = models.DateTimeField('date executed')
+    date        = models.DateTimeField(auto_now_add=True, blank=True)
     input_data  = models.TextField()
     output_data = models.TextField()
+    def __unicode__(self):
+        return self.name
