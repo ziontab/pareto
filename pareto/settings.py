900cfb8dd293a50ec87e749eb13580f4db06904c
Dinar Sabitov <lolall@yandex.ru>
2014-05-11 19:36:58 +0400

    fix config

--- pareto/settings.py
+++ pareto/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 LOGIN_URL = '/login/'
 
 # Local time zone for this installation. Choices can be found here:
