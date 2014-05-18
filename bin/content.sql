c43ccfce284656c5b6cef61b0bad468b7bae5179
Dinar Sabitov <lolall@yandex.ru>
2014-05-18 18:38:12 +0400

    add config and migration

---
+++ bin/content.sql
@@ -0,0 +1,27 @@
+INSERT INTO parapp_indicator (name, value, date, is_binary) VALUES
+("hypervolume", "hypervolume", NOW(), 0),
+("epsilon+", "epsilon+", NOW(), 0),
+("epsilon multiplicate", "epsilon multiplicate", NOW(), 0),
+("R1", "R1", NOW(), 0),
+("R2", "R2", NOW(), 0),
+("R3", "R3", NOW(), 0);
+
+INSERT INTO parapp_algorithm (name, value, date) VALUES
+("spea2", "spea2", NOW()),
+("nsga2", "nsga2", NOW()),
+("ibea", "ibea", NOW()),
+("femo", "femo", NOW());
+
+INSERT INTO parapp_problem (name, value, date) VALUES
+("zdt1", "zdt1", NOW()),
+("zdt2", "zdt2", NOW()),
+("zdt3", "zdt3", NOW()),
+("zdt4", "zdt4", NOW()),
+("zdt6", "zdt6", NOW()),
+("dtlz1", "dtlz1", NOW()),
+("dtlz2", "dtlz2", NOW()),
+("dtlz3", "dtlz3", NOW()),
+("dtlz4", "dtlz4", NOW()),
+("dtlz5", "dtlz5", NOW()),
+("dtlz6", "dtlz6", NOW()),
+("dtlz7", "dtlz7", NOW());
